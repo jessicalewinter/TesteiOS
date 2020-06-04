dcb1f2b8b00932b813e7351f0914f576cba56e6a
Jessica Lewinter <jessicalewinter@gmail.com>
2020-06-04 01:31:04 -0300

    fix: Change the structure of the project by deleting storyboard and adding gitignore/swiflint

---
+++ InvestmentApp/InvestmentApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  InvestmentApp
+//
+//  Created by Jessica Lewinter on 04/06/20.
+//  Copyright © 2020 Jessica Lewinter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+}

a2d3a9d55d2a03b7d583ea3b2593807b958557ec
Jessica Lewinter <jessicalewinter@gmail.com>
2020-06-09 00:45:41 -0300

    feature network layer: Created framework and added helper enums

---
+++ InvestmentApp/NetworkLayer/NetworkLayer/Enums/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  NetworkLayer
+//
+//  Created by Jessica Lewinter on 09/06/20.
+//  Copyright © 2020 Jessica Lewinter. All rights reserved.
+//
+
+import Foundation
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
